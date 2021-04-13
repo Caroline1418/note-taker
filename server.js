ee52deb236dce873ae643ee8595dc0c38ccc72e7
Caroline Phillips <Caroline@Carolines-MacBook-Air.local>
2021-04-12 18:28:12 -0600

    pls work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 // const path = require('path');
 // const fs = require('fs');
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3005;
 
 //Testing, not really needed here
 const dbJson = require('./db/db.json')
